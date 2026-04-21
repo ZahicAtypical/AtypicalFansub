5226fc4dddddc590557e1a2abd93124c90e05a29
Zahic@Atypical <AtypicalZahic@gmail.com>
2026-04-21 18:34:38 +0800

    AtypicalFansubTestbranch_v1_触发 Vercel 构建

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Atypical',
+			title: 'AtypicalFansub',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
